13ef1e808686cfaed2694fa13308918e8f7e49f5
Pallav34 <Programmer@example.com>
2024-07-20 23:07:51 +0530

    socket and frontend url updated

--- client/src/context/SocketContext.jsx
+++ client/src/context/SocketContext.jsx
@@ -9,7 +9,8 @@ export const SocketContextProvider = ({ children }) => {
   const [socket, setSocket] = useState(null);
 
   useEffect(() => {
-    setSocket(io("http://localhost:4000"));
+    //setSocket(io("http://localhost:4000"));
+    setSocket(io("https://nest-finder-socket.vercel.app"));
   }, []);
 
   useEffect(() => {
--- socket/app.js
+++ socket/app.js
@@ -3,7 +3,7 @@ import { Server } from "socket.io";
 const io = new Server({
   cors: {
     // origin: "http://localhost:5173",
-    origin: "https://nest-finder-six.vercel.app/",
+    origin: "https://nest-finder-six.vercel.app",
   },
 });
 
